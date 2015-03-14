95fe5319698f005f9a35a066d57e518f112aa130
Stephanie Barlock <stephanie.barlock@db.com>
2015-03-13 22:34:03 -0400

    Added filtering to the guest list view - can now view by who has RSVP'ed (and what they chose) and by whose side they are on.

--- public/js/guests/controllers/guestListController.js
+++ public/js/guests/controllers/guestListController.js
@@ -9,6 +9,8 @@ define([
     module.controller(name, ['$scope', namespace + '.GuestServices', function ($scope, GuestServices) {
         $scope.guests = [];
         $scope.alerts = [];
+        $scope.guestFilter = {};
+        $scope.groupFilter = {};
 
         $scope.closeAlert = function (index) {
             $scope.alerts.splice(index, 1);
@@ -32,17 +34,12 @@ define([
 
         $scope.addGuest = function (newGuest) {
             var alerts = validateGuest(newGuest);
+            newGuest.rsvpd = false;
 
             if (newGuest && alerts.length === 0) {
-                GuestServices.save(newGuest).$promise.then(
-                    function success(data) {
-                        newGuest._id = data.id;
-                        $scope.guests.push(newGuest);
-                    },
-                    function error(data) {
-                        $scope.alerts.push({msg: data, type: "danger"});
-                    }
-                );
+                GuestServices.save(newGuest, function () {
+                    $scope.guests.push(newGuest);
+                });
             } else {
                 $scope.alerts = $scope.alerts.concat(alerts);
             }
@@ -55,18 +52,13 @@ define([
             if (updatedGuest && alerts.length === 0) {
                 guestId = updatedGuest._id;
 
-                GuestServices.update({ id:guestId }, updatedGuest).$promise.then(
-                    function success() {
-                        angular.forEach($scope.guests, function (guest, index) {
-                            if (guest._id === guestId) {
-                                $scope.guests[index] = updatedGuest;
-                            }
-                        })
-                    },
-                    function error(data) {
-                        $scope.alerts.push({msg: data, type: "danger"});
-                    }
-                );
+                GuestServices.update({id: guestId}, updatedGuest, function () {
+                    angular.forEach($scope.guests, function (guest, index) {
+                        if (guest._id === guestId) {
+                            $scope.guests[index] = updatedGuest;
+                        }
+                    });
+                });
             } else {
                 $scope.alerts = $scope.alerts.concat(alerts);
             }
@@ -75,21 +67,59 @@ define([
         $scope.deleteGuest = function (guest) {
             var guestId = guest._id;
             if (guestId) {
-                GuestServices.delete({ id:guestId }).$promise.then(
-                    function success() {
-                        angular.forEach($scope.guests, function (guest, index) {
-                            if (guest._id === guestId) {
-                                $scope.guests.splice(index, 1);
-                            }
-                        })
-                    },
-                    function error(data) {
-                        $scope.alerts.push({msg: data, type: "danger"});
-                    }
-                );
+                GuestServices.delete({id: guestId}, function () {
+                    angular.forEach($scope.guests, function (guest, index) {
+                        if (guest._id === guestId) {
+                            $scope.guests.splice(index, 1);
+                        }
+                    });
+                });
             }
         };
 
+        $scope.attendingGreaterThan = function (value) {
+            return function (item) {
+                return item.numberComing > value;
+            };
+        };
+
+        $scope.attendingEquals = function (value) {
+            return function (item) {
+                return item.numberComing === value;
+            };
+        };
+
+        $scope.setActive = function (tab) {
+            $scope.activeTab = tab.title;
+            $scope.guestFilter = tab.filter;
+        }
+
+        $scope.isActive = function(title) {
+            return title == $scope.activeTab;
+        }
+
+        $scope.hasRsvpd = function() {
+            return $scope.isActive('Attending') || $scope.isActive('Not Attending');
+        }
+
+        $scope.attending = function() {
+            return $scope.isActive('Attending');
+        }
+
         $scope.guests = GuestServices.query();
+        $scope.activeTab = 'All';
+        $scope.tabs = [{
+            title: 'All',
+            filter: {}
+        }, {
+            title: 'Attending',
+            filter: $scope.attendingGreaterThan(0)
+        }, {
+            title: 'Not Attending',
+            filter: $scope.attendingEquals(0)
+        }, {
+            title: 'Awaiting Response',
+            filter: {rsvpd:false}
+        }];
     }])
 });
